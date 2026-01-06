8fb82a9c0c1646f9660352e154e7b6e0ca8a3ed3
leonishard <lenny.richartz@outlook.com>
2026-01-06 12:38:25 +0100

    Reapply "Playerstats"
    This reverts commit 42b77b082830fc30d2fbe397c5613f5875bf0304.

--- Assets/Player/Shooting.cs
+++ Assets/Player/Shooting.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -7,41 +5,69 @@ public class Shooting : MonoBehaviour
     private Camera mainCam;
     private Vector3 mousePos;
 
+    private PlayerStats stats;
+
     public GameObject bullet;
     public Transform bulletTransform;
-    public bool canFire;
+
+    public bool canFire = true;
     private float timer;
-    public float timeBetweenFiring;
+    public float timeBetweenFiring = 0.2f;
+
+    [Header("Spread")]
+    public float spreadDegrees = 8f;
 
     void Start()
     {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        mainCam = Camera.main;
+        stats = GetComponentInParent<PlayerStats>(); // Shooting is on child/pivot, stats is on Player
     }
 
     void Update()
     {
-        // Rotate towards mouse
+        // Rotate this pivot (square) towards mouse (YOUR OLD BEHAVIOR)
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
         Vector3 rotation = mousePos - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
 
-        // Fire cooldown timer
+        // Cooldown timer
         if (!canFire)
         {
             timer += Time.deltaTime;
-            if (timer > timeBetweenFiring)
+            if (timer >= timeBetweenFiring)
             {
                 canFire = true;
-                timer = 0;
+                timer = 0f;
             }
         }
 
-        // Shooting
+        // Fire
         if (Input.GetMouseButton(0) && canFire)
         {
             canFire = false;
-            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        int count = stats ? stats.projectileCount : 1;
+        int dmg = stats ? stats.Damage : 1;
+
+        Quaternion baseRot = transform.rotation;
+
+        for (int i = 0; i < count; i++)
+        {
+            float angleOffset = (count == 1) ? 0f : spreadDegrees * (i - (count - 1) / 2f);
+            Quaternion rot = baseRot * Quaternion.Euler(0f, 0f, angleOffset);
+
+            GameObject b = Instantiate(bullet, bulletTransform.position, rot);
+
+            BulletScript bs = b.GetComponent<BulletScript>();
+            if (bs != null) bs.damage = dmg;
         }
     }
 }
